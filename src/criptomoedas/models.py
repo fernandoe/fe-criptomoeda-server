355297c0b33d5bb05876ec81ffcec259c7db64d1
Fernando Espíndola <fernando.espindola@hp.com>
2018-02-27 18:40:24 -0300

    WIP

--- src/criptomoedas/models.py
+++ src/criptomoedas/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from fe_core.models import UUIDModel
+
+
+class Moeda(UUIDModel):
+    cep = models.CharField(max_length=10)
+    logradouro = models.CharField(max_length=128)
+    numero = models.CharField(max_length=32)
+    complemento = models.CharField(max_length=32, null=True, blank=True)
+    bairro = models.CharField(max_length=56)
+    cidade = models.CharField(max_length=56)
+    estado = models.CharField(max_length=2)
+
--- src/criptomoedas/views.py
+++ src/criptomoedas/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from myapp.serializers import UserSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
 
-# Create your views here.
-def example(request):
-    return HttpResponseRedirect(reverse('news-year-archive', args=(year,)))
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAdminUser,)
